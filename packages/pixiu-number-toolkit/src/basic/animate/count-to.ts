8539225308e5ee8e9194736778a042f5a608311e
mux <55856773@qq.com>
2024-01-23 18:35:11 +0800

    fix: countTo

--- packages/pixiu-number-toolkit/src/basic/animate/count-to.ts
+++ packages/pixiu-number-toolkit/src/basic/animate/count-to.ts
@@ -1,18 +1,22 @@
+import { computeExpression } from "../math/compute-expression";
+
 export function countTo(
   start: number,
   end: number,
   duration: number,
   callback: (value: number) => void,
 ) {
-  const difference = end - start;
-  const perTick = (difference / duration) * 10;
+  const difference = Number(computeExpression(`${end} - ${start}`));
+  const perTick = Number(
+    computeExpression(`(${difference} / ${duration}) * 10`),
+  );
 
   let current = start;
   let elapsed = 0;
 
   const tick = () => {
-    elapsed += 10;
-    current += perTick;
+    elapsed = Number(computeExpression(`${elapsed}+ 10`));
+    current = Number(computeExpression(`${current} +${perTick}`));
 
     if (elapsed >= duration) {
       callback(end);
