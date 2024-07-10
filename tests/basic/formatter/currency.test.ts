9369c09c7056d758e5a6e399e41754f4b5c203a0
mux <55856773@qq.com>
2024-07-10 10:39:56 +0800

    fix: remove formatCurrencyRMB test

--- tests/basic/formatter/currency.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from '@jest/globals';
-import { formatCurrencyRMB } from '../../../src';
-
-describe('formatter', () => {
-  it('formatCurrencyRMB', () => {
-    expect(formatCurrencyRMB(123_456_789)).toBe('¥123,456,789.00');
-  });
-});
