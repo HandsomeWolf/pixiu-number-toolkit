2a8af07fb2d21e8b837863baa3dc9e62a8fdb7d2
mux <55856773@qq.com>
2024-07-11 14:42:26 +0800

    test: format

--- tests/basic/utils/id-card.test.ts
+++ tests/basic/utils/id-card.test.ts
@@ -1,27 +1,27 @@
-import {describe, it, expect} from '@jest/globals'
-import { parseIdCard } from "../../../src";
+import { describe, it, expect } from '@jest/globals';
+import { parseIdCard } from '../../../src';
 
-describe("parseIdCard", () => {
-  describe("valid id card", () => {
-    it("valid is true", () => {
-      expect(parseIdCard("110105197208108139")).toStrictEqual({
-        birthdate: "1972-08-10",
-        gender: "男",
-        region: "110105",
+describe('parseIdCard', () => {
+  describe('valid id card', () => {
+    it('valid is true', () => {
+      expect(parseIdCard('110105197208108139')).toStrictEqual({
+        birthDate: '1972-08-10',
+        gender: '男',
+        region: '110105',
         valid: true,
       });
-      expect(parseIdCard("11010519751030611X")).toStrictEqual({
-        birthdate: "1975-10-30",
-        gender: "男",
-        region: "110105",
+      expect(parseIdCard('11010519751030611X')).toStrictEqual({
+        birthDate: '1975-10-30',
+        gender: '男',
+        region: '110105',
         valid: true,
       });
     });
-    it("valid is false", () => {
-      expect(parseIdCard("11010119900307861X")).toStrictEqual({
-        birthdate: "",
-        gender: "",
-        region: "",
+    it('valid is false', () => {
+      expect(parseIdCard('11010119900307861X')).toStrictEqual({
+        birthDate: '',
+        gender: '',
+        region: '',
         valid: false,
       });
     });
